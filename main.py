c0578fccd8774e457e82bfed425494a66b14d1fb
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2023-12-12 11:55:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     print(fract.__sub__(Fraction(1,5))) # fract - 1/5 = 17/15
     print(Fraction(1,2).is_integer()) #false
     print(fract2.__add__(fract)) #25/12
+    try: #Other must be an instance of class Fraction
+        print(fract.__mul__(8/4))
+    except Exception as error :
+        print(error)
+
 
 
 if __name__ == "__main__":
