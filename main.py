57a43e586238d661c3407392ef0a0b1580e477e2
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2023-12-12 11:50:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from classeFraction import Fraction
 
 def main():
-    f=Fraction(8,6)
-    f2=Fraction(3,4)
-    print(f.__sub__(Fraction(1,5)))
+    fract=Fraction(8,6)
+    fract2=Fraction(3,4)
+    print(fract.__sub__(Fraction(1,5))) # fract - 1/5 
     print(Fraction(1,2).is_integer())
 
 
